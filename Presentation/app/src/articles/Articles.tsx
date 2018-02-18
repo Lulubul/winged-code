15826cbe0201cc90a65fabfab90af5cc6e1a96f3
Lulubul <robert.unguru@gmail.com>
2018-02-18 14:17:09 +0200

    Add dummy articles

--- Presentation/app/src/articles/Articles.tsx
+++ Presentation/app/src/articles/Articles.tsx
@@ -9,6 +9,7 @@ import MoreVertIcon from 'material-ui-icons/MoreVert';
 const styles = ({
   card: {
     maxWidth: 400,
+    marginTop: 25
   },
   media: {
     height: 194,
@@ -29,43 +30,71 @@ const styles = ({
   },
 });
 
+interface Article {
+  index: number;
+  title: string;
+  subheader: string;
+  image: string;
+  summary: string;
+}
+
 export default class Articles extends React.Component {
 
   render() {
-    const articles = [1, 2, 3].map(x => this.createCard(x));
+
+    const articles: Article[] = [
+      {
+        index: 1,
+        title: 'Intro into ASP.NET CORE 2',
+        subheader: 'January 14, 2018',
+        image: 'https://goo.gl/dWF3c8',
+        summary: `NET Core MVC with controllers and views. Razor Pages is a new alternative in ASP.NET Core 2.0,
+                  a page-based programming model that makes building web UI easier and more productive.`
+      },
+      {
+        index: 2,
+        title: 'Micro Applications and Microservices Together',
+        subheader: 'January 18, 2018',
+        image: 'https://goo.gl/5jJvUy',
+        summary: `This post discusses the integration between rich-UI and backend services. 
+                  A rich-UI is decomposed as multiple logical pieces, each called a micro-application.
+                  The backend services are similarly broken into multiple pieces as microservices.`
+      },
+      {
+        index: 3,
+        title: 'Progressive Web Apps',
+        subheader: 'January 24, 2018',
+        image: 'https://goo.gl/dWfuVF',
+        summary: `A new way to deliver amazing user experiences on the web.`
+      }
+    ];
+
     return (
       <Grid container={true} justify={'center'}>
-        <Grid item={true} style={{ padding: 20 }}>
-          {articles}
+        <Grid item={true}>
+          {articles.map(article => this.createCard(article))}
         </Grid>
       </Grid>
     );
   }
 
-  private createCard = (index: number) => {
+  private createCard = (article: Article) => {
     return (
-      <Card style={styles.card} key={index}>
+      <Card style={styles.card} key={article.index}>
         <CardHeader
           avatar={<Avatar aria-label="Recipe" style={styles.avatar}> D </Avatar>}
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="Intro into ASP.NET CORE 2"
-          subheader="September 14, 2018"
+          action={<IconButton><MoreVertIcon /></IconButton>}
+          title={article.title}
+          subheader={article.subheader}
         />
         <CardMedia
           style={styles.media}
-          // tslint:disable-next-line:max-line-length
-          image="http://www.gokhan-gokalp.com/wp-content/uploads/2017/06/ASP.NET-Core-Logo_2colors_Boxed_RGB_bitmap_BIG.png"
-          title="Contemplative Reptile"
+          image={article.image}
         />
         <CardContent>
           <Typography component="p">
-            NET Core MVC with controllers and views. Razor Pages is a new alternative in ASP.NET Core 2.0,
-            a page-based programming model that makes building web UI easier and more productive.
-      </Typography>
+            {article.summary}
+          </Typography>
         </CardContent>
         <CardActions style={styles.actions} disableActionSpacing={true}>
           <IconButton aria-label="Add to favorites">
