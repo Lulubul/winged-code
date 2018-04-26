bc4078d729dae641b260db024358f856f19f622a
Lulubul <robert.unguru@gmail.com>
2018-04-26 13:41:49 +0300

    Add HCI article

--- Presentation/app/src/articles/Articles.tsx
+++ Presentation/app/src/articles/Articles.tsx
@@ -12,6 +12,7 @@ require('firebase/firestore');
 import core from './images/core.png';
 import microservices from './images/microservices.png';
 import pwa from './images/pwa.png';
+import hci from './images/hci.png';
 import { StyleRules } from 'material-ui/styles';
 
 const articleStyle: StyleRules = {
@@ -72,6 +73,55 @@ class Articles extends React.Component<PropsWithStyles, ArticlesState> {
     return (
       <Grid container={true} justify={'center'} spacing={24}>
         <Grid item={true} xs={12} style={articleStyle.item}>
+        <Card key={0} style={articleStyle.card}>
+          <CardHeader
+            avatar={<Avatar aria-label="Recipe" style={articleStyle.avatar}> D </Avatar>}
+            action={<IconButton><MoreVertIcon /></IconButton>}
+            title="Human computer interaction"
+            subheader="project, infoiasi, hci"
+          />
+          <CardMedia
+            style={articleStyle.media}
+            image={hci}
+          />
+          <CardContent>
+            <Typography component="p">
+              I'm currently working on one educational project that involves Human computer interaction.
+              The project is trying to resurrect Street Figher II with <a href="https://www.vuforia.com/">Vuforia</a>
+              &nbsp;and &nbsp;<a href="https://unity3d.com/">Unity3d</a> using interaction design.
+            </Typography>
+            <hr/>
+            <Typography component="p">
+              Before doing anything at the project I've invested some time in reading 
+              about Information Architecture and Visual Design.
+              Also I've invested some days to discover more about branding and personas.
+              After that I've started with QOC (Questions, options and criteria) and with 3 ideas 
+              putted in 3 wireframes.
+            </Typography>
+            <hr/>
+            <Typography component="p">
+              After the documentation was complete I've decided to investigate 
+              &nbsp;<a href="https://proto.io/">Proto.io</a> .
+              A nice tool that allow you to create prototypes and lucky 
+              for me he has material design as default package.
+              So I've create a prototype for the app based on my wireframes using this cool tool. 
+              &nbsp;<a href="https://danielunguru.proto.io/projects/"> Take a look... </a>
+            </Typography>
+            <hr/>
+            <Typography component="p">
+              Follow the project progress at <a href="https://hci.gitbook.io/gamion/">GAMICON Gitbook</a> or&nbsp; 
+              <a href="https://github.com/Lulubul/HCI">GAMICON github</a>
+            </Typography>
+          </CardContent>
+          <CardActions style={articleStyle.actions} disableActionSpacing={true}>
+            <IconButton aria-label="Add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="Share">
+              <ShareIcon />
+            </IconButton>
+          </CardActions>
+        </Card>
           {articles.map((article, index) => this.createCard(index, article))}
         </Grid>
       </Grid>
